a3a7ad1d104b7dbb2d9d5ba11dc89427e21ea540
V3loz4 <joanavelosa@gmail.com>
2018-09-23 16:57:05 +0200

    Changed solution for Naive

--- Implementation/Naive2.cpp
+++ Implementation/Naive2.cpp
@@ -3,7 +3,7 @@
 
 bool Naive2::parse(string word)
 {
-	int tamWord = word.size(); // word and wordNaive22 have the same length
+	int tamWord = word.size(); // word and wordNaive2 have the same length
 	wordNaive2 = new char[tamWord];
 	for (int i = 0; i < tamWord; i++)
 	{
@@ -29,20 +29,24 @@ bool Naive2::parse(char A, int i, int j)
 
 	for (int a = 0; a < numEntries; a++)
 	{
-		if (parArray[a].second == null)
+		//if (parArray[a].second == null)
+		//{
+		//	if (parArray[a].first == (wordNaive2[i])) return true;
+		//}
+		//else
+		//{
+		// if I got it right you are checking terminals above but this should only be done when ending the recursion like above 
+		// in the first case because if you choose a terminal rule here and it returns true you can not go further down in the "tree
+		// for the rules" so you can not check for the rest of the string if it really fits to the grammar  
+		// other then that I coult not find anything different to my code but also my structure for the grammar is a bit different so I am 
+		// not 100 percent sure if there are other bugs. I hope it helps to compare it with my code. 
+
+		int k = i + 1;
+		for (k; k < j; k++)
 		{
-			if (parArray[a].first == (wordNaive2[i])) return true;
-			if (parArray[a].first == (wordNaive2[i])) return true;
-			if (parArray[a].first == (wordNaive2[i])) return true;
+			if (parse(parArray[a].first, i, k) && parse(parArray[a].second, k, j)) return true;
 		}
-		else
-		{
-			int k = i + 1;
-			for (k; k < j; k++)
-			{
-				if (parse(parArray[a].first, i, k) && parse(parArray[a].second, k, j)) return true;
-			}
-		}
+		//}
 	}
 	return false;
 }
